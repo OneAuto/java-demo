2f148aaa229a2cdb7791adc299f44220ac34da7c
Ant <1596863112@qq.com>
2020-01-09 00:24:30 +0800

    JavaCode

--- src/main/java/io/filestream/DataStream.java
+++ src/main/java/io/filestream/DataStream.java
@@ -1,5 +1,6 @@
 package io.filestream;
 
+import org.omg.PortableInterceptor.INACTIVE;
 import org.slf4j.Logger;
 import sun.rmi.runtime.Log;
 import utils.LoggerUtils;
@@ -10,9 +11,52 @@ public class DataStream {
     public static final Logger LOG=LoggerUtils.getLoggerEntity(DataStream.class);
 
     public static void main(String[] args) {
-        writer();
-        read();
+        method1();
+        method2();
     }
+
+    public static void method1(){
+        File file=new File("/Volumes/Backup/test.txt");
+        try(
+                FileOutputStream fos=new FileOutputStream(file);
+                FileInputStream fis=new FileInputStream(file);
+                DataInputStream dis=new DataInputStream(fis);
+                DataOutputStream dos=new DataOutputStream(fos);
+                ){
+            dos.writeInt(15);
+            dos.writeInt(31);
+            dos.flush();
+
+            int a=dis.readInt();
+            int b=dis.readInt();
+            LOG.info("使用数据流读取出的数据 a={},b={}",a,b);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void method2()  {
+        File file=new File("/Volumes/Backup/text.txt");
+        try(
+                FileWriter fw=new FileWriter(file);
+                PrintWriter pw=new PrintWriter(fw);
+                FileReader fr=new FileReader(file);
+                BufferedReader br=new BufferedReader(fr);
+            ){
+                fw.write(15+"@"+31);
+                fw.flush();
+
+                String str=br.readLine();
+                String [] all=str.split("@");
+                int a=Integer.parseInt(all[0]);
+                int b=Integer.parseInt(all[1]);
+                LOG.info("使用缓存流读取出的数据 a={},b={}",a,b);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     public static void read()  {
         File file=new File("/Volumes/Backup/dos.txt");
