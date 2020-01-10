4ac848b135487d1aff4ea159a4f18651fd4fd002
Ant <1596863112@qq.com>
2020-01-10 15:23:26 +0800

    JavaCode

--- src/main/java/net/TestSocket.java
+++ src/main/java/net/TestSocket.java
@@ -3,21 +3,19 @@ package net;
 import org.slf4j.Logger;
 import utils.LoggerUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class TestSocket {
-    private static String ip=null;
 
     private static final Logger LOG= LoggerUtils.getLoggerEntity(TestSocket.class);
 
     public static void main(String[] args) {
+        getLocalIp();
     }
 
     public static String getLocalIp(){
+        String ip=null;
         try {
             InetAddress host=InetAddress.getLocalHost();
             ip=host.getHostAddress();
