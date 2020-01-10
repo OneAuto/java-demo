ea5a538d16c5ea3253d6509642b327f918e89fd8
Ant <1596863112@qq.com>
2020-01-10 16:42:06 +0800

    JavaCode

--- src/main/java/net/TestSocket.java
+++ src/main/java/net/TestSocket.java
@@ -3,6 +3,9 @@ package net;
 import org.slf4j.Logger;
 import utils.LoggerUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -11,7 +14,23 @@ public class TestSocket {
     private static final Logger LOG= LoggerUtils.getLoggerEntity(TestSocket.class);
 
     public static void main(String[] args) {
-        getLocalIp();
+        getPingLocalInfo();
+    }
+
+    public static void getPingLocalInfo() {
+
+        Process p= null;
+        try (BufferedReader br=new BufferedReader(new InputStreamReader(p.getInputStream()));){
+            p = Runtime.getRuntime().exec("ping "+getLocalIp());
+            //造流，对流进行操作
+            LOG.info("本次Ping返回的消息：");
+            while(br.read()!=-1){
+                LOG.info(br.readLine()+"\r");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static String getLocalIp(){
--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,31 +1,62 @@
 package thread;
 
+import javafx.application.Platform;
 import org.slf4j.Logger;
 import utils.LoggerUtils;
 
 public class Main {
     private static final Logger LOG= LoggerUtils.getLoggerEntity(Main.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+
+
+        /*
+        创建线程的三种方式：
+        1.继承Thread类
+        2.实现Runnable接口
+        3.匿名类方式
+         */
+
+        //实现Runnable接口
        Thread th= new Thread(new Runnable() {
             @Override
             public void run() {
-                LOG.info("启动了新线程");
+
                 try {
-                    Thread.sleep(1000);
+                    //当前线程暂停3秒
+                    Thread.sleep(3000);
+                    LOG.info("th线程");
+                    LOG.info("th线程执行完毕->耗时3000ms");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
+
+
+        //匿名类
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    //当前线程暂停1秒
+                    Thread.sleep(1000);
+                    LOG.info(getName()+ "线程");
+                    LOG.info(getName()+" 线程执行完毕->耗时1000ms");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        thread.setPriority(Thread.MAX_PRIORITY);        //设置优先级最大
+        th.setPriority(Thread.MIN_PRIORITY);        //设置优先级最小
+        thread.join();      //加入主线程，等该线程完成后才能继续
+        th.join();
+        thread.start();
         th.start();
 
-        try{
-            //加入主线程，要等th线程执行完后才继续下去
-            th.join();
-            LOG.info("th线程执行完毕->耗时1000ms");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        LOG.info("主线程最后执行");
     }
 }
