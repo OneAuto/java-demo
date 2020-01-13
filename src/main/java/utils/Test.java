e776fde5e83641be985c9beb01e07d0b1564f679
Ant <1596863112@qq.com>
2020-01-13 19:45:32 +0800

    JavaCode

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -1,9 +1,6 @@
 package utils;
-
-import java.sql.Connection;
-
 public class Test {
     public static void main(String[] args) {
-        Connection conn=C3p0Utils.getConnection();
+        
     }
 }
--- src/main/java/utils/c3p0/C3p0Utils.java
+++ src/main/java/utils/c3p0/C3p0Utils.java
@@ -1,13 +1,14 @@
-package utils;
+package utils.c3p0;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.slf4j.Logger;
+import utils.LoggerUtils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class C3p0Utils {
-    private static final Logger LOG=LoggerUtils.getLoggerEntity(C3p0Utils.class);
+    private static final Logger LOG= LoggerUtils.getLoggerEntity(C3p0Utils.class);
     private static ComboPooledDataSource comboPooledDataSource=new ComboPooledDataSource("mysql5.7");
 
     public static Connection getConnection(){
