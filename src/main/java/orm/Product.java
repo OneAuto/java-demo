145af7a4dce7dc0487e50e5b3fef9fc168cd4577
Ant <1596863112@qq.com>
2020-01-14 13:25:18 +0800

    JavaCode

---
+++ src/main/java/orm/Product.java
@@ -0,0 +1,57 @@
+package orm;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_", schema = "test", catalog = "")
+public class Product {
+    private int id;
+    private String name;
+    private Double price;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 30)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price", nullable = true, precision = 0)
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
