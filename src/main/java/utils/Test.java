de9e30849445b633658f8f7a2117f1d3ede658d3
Ant <1596863112@qq.com>
2020-01-14 12:09:23 +0800

    JavaCode

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -6,17 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         C3p0Utils c=new C3p0Utils();
         Connection conn=c.getConnection();
         try {
-            PreparedStatement ps =conn.prepareStatement("select * from ");
+            PreparedStatement ps =conn.prepareStatement("select * from product_");
             ResultSet rs=ps.executeQuery();
             while (rs.next()){
                 System.out.println(rs.getString(1));
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            conn.close();
         }
     }
 }
