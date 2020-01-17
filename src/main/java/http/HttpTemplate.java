0ffd98572ec352dc4c06ca8e0c1ad2b6220c8a54
Ant <1596863112@qq.com>
2020-01-17 23:25:52 +0800

    JavaCode

--- src/main/java/http/HttpTemplate.java
+++ src/main/java/http/HttpTemplate.java
@@ -1,7 +1,7 @@
 package http;
 
-import com.alibaba.fastjson.JSONObject;
 import com.github.kevinsawicki.http.HttpRequest;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import utils.LoggerUtils;
 
@@ -13,19 +13,18 @@ import java.net.HttpURLConnection;
 public class HttpTemplate {
     static final Logger LOG= LoggerUtils.getLoggerEntity(HttpTemplate.class);
     private final static String URL = "http://jw.nnxy.cn/app.do?method=authUser&xh=20181004095&pwd=fengge123";
-    private final static String url="http://jw.nnxy.cn/app.do?method=getCjcx&xh=20181004095&xnxqid=2019-2020-1";
+    private final static String url="http://jw.nnxy.cn/app.do?method=getCjcx&xh=20181004116";
     public static void main(String[] args) {
         String response= HttpRequest.get(URL).body();
-        LOG.info("请求返回json串:{}",response);
-
-        JSONObject json=JSONObject.parseObject(response);
+        LOG.info("json：{}",response);
+        JSONObject json=JSONObject.fromObject(response);
         String token= (String) json.get("token");
-        LOG.info("token:{}",token);
+
         try {
           HttpURLConnection conn= HttpRequest.get(url).getConnection();
           conn.setRequestProperty("token",token);
             BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            System.out.println(in.readLine());
+            LOG.info("content：{}",in.readLine());
         } catch (HttpRequest.HttpRequestException | IOException e) {
             e.printStackTrace();
         }
